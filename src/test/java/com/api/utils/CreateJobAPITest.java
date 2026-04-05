c6f8f4705a72adb11f00bcaadae6fdff7b2da0cc
Akshay <akshayh1247@gmail.com>
2026-04-05 21:15:08 +0530

    Introduced Enums for handling the code for the create job api test Reduced Magic Number Issue in the test script

--- src/test/java/com/api/utils/CreateJobAPITest.java
+++ src/test/java/com/api/utils/CreateJobAPITest.java
@@ -7,7 +7,15 @@ import java.util.List;
 
 import org.testng.annotations.Test;
 import static com.api.utils.DateTimeUtil.*;
+
+import com.api.constant.Model;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
 import com.api.constant.Role;
+import com.api.constant.ServiceLocation;
+import com.api.constant.Warranty_status;
 import com.api.request.model.CreateJobPayload;
 import com.api.request.model.Customer;
 import com.api.request.model.CustomerAddress;
@@ -26,11 +34,12 @@ public class CreateJobAPITest {
 		
 		Customer customer = new Customer("Akshay","H" ,"9809809800" ,"","saitama@yopmail.com" , "" );
 		CustomerAddress customerAddress = new CustomerAddress("Mumbai", "Mumbai", "Mumbai","Mumbai", "Mumbai","400703", "India", "Maharashtra");
-		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "68679856619518", "68679856619518", "68679856619518", getTimeWithDaysAgo(10), 3, 3);
-		Problems problems = new Problems(2, "Battery Health");
+		CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10), "686798561951218", "686798561951218", "686798561951218", getTimeWithDaysAgo(10), 
+				Product.IPHONE.getCode(), Model.IPHONE_11.getCode());
+		Problems problems = new Problems(Problem.POOR_BATTERY_LIFE.getCode(), "Battery Health");
 		List<Problems> problemsList = new ArrayList<Problems>();
 		problemsList.add(problems);
-		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 2, customer, customerAddress, customerProduct, problemsList);
+		CreateJobPayload createJobPayload = new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warranty_status.IN_WARRANTY.getCode(), OEM.APPLE.getCode(), customer, customerAddress, customerProduct, problemsList);
 		
 		given()
 			.spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload))
